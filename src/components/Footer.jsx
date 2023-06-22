5564b463bb0d1a1b8449108ce5864f9961234e7f
Amr Elskhawy <amrelskhawy@gmail.com>
2023-06-22 22:28:36 +0200

    Creats Footer Component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,47 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
-  console.log('Footer');
+  const socialContact = [
+    {
+      id: 1,
+      path: "/1.png",
+      alt: 'facebook page',
+      href: ''
+    },
+    {
+      id: 2,
+      path: "/2.png",
+      alt: 'Instgram page',
+      href: ''
+    },
+    {
+      id: 3,
+      path: "/3.png",
+      alt: 'Twitter page',
+      href: ''
+    },
+    {
+      id: 4,
+      path: "/4.png",
+      alt: 'Youtube Channel',
+      href: ''
+    },
+  ]
   return (
-    <div>
-      <div>
+    <div className='h-12 flex items-center justify-between'>
+      <div className='text-lg'>
         &copy;2023 Bloggy!. All Rights Reserved.
       </div>
-      <div></div>
+      <div className='flex justify-center items-center gap-4'>
+        {
+          socialContact.map(ele => (
+            <Image
+              className='opacity-60 cursor-pointer hover:opacity-100 transition-all'
+              width={20} height={20} key={`Social ${ele.id}`} src={ele.path} alt={ele.alt} />
+          ))
+        }
+      </div>
     </div>
   )
 }
