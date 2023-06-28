55130672652bf4180276d6a102f82bca30518ca0
Amr Elskhawy <amrelskhawy@gmail.com>
2023-06-28 03:18:51 +0300

    Adds getData Func to fetch data from server to blog page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,19 +1,44 @@
-"use client"
 
 import React from "react";
 import Image from "next/image";
+import { notFound } from "next/navigation"
 
-const BlogPost = ({ params }) => {
+const getData = async ({ id }) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${id}`, {
+      cache: "no-store"
+    })
+
+  if (!res.ok) {
+    // return notFound()
+  }
+  return res.json()
+}
+
+export async function generateMetadata({ params, searchParams }, parent) {
+  // read route params
+ 
+  // fetch data
+  const post = await getData(params)
+  
+ 
+  return {
+    title: post.title,
+  }
+}
+ 
+const BlogPost = async ({ params }) => {
+
+  const data = await getData(params)
+  console.log(data.title)
   return (
     <div className="flex flex-col ">
       <div className="flex gap-4 mb-8">
-        <div className=" flex flex-col   justify-between">
+        <div className=" flex-1 flex flex-col justify-between">
           <h1 className="text-[36px] font-bold">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. </h1>
+            {data.title} </h1>
           <br />
           <p className="text-xl font-[300]">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis iste quaerat porro labore eaque nostrum consequuntur recusandae molestiae est numquam, corrupti excepturi repudiandae similique amet temporibus architecto doloremque sunt ipsa.
-
+            {data.desc}
 
           </p>
           <div className="flex items-center relative gap-3">
@@ -22,20 +47,16 @@ const BlogPost = ({ params }) => {
               alt=""
               height={40}
               width={40}
-              
+
               className=" object-cover relative h-12 w-12 rounded-full"
             />
-            <span className="inline-block">John Doe</span>
+            <span className="inline-block">{data.username}</span>
           </div>
         </div>
 
-
-
-
-
-        <div className="h-[300px] w-full relative">
+        <div className="h-[300px] w-full  flex-1 relative">
           <Image
-            src="https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            src={data.img}
             alt=""
             loading="lazy"
             fill={true}
@@ -43,15 +64,9 @@ const BlogPost = ({ params }) => {
           />
         </div>
       </div>
-
-
       <div className="flex-1">
         <p className="mt-13 text-xl font-[300] text-[#999] text-justify">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis iste quaerat porro labore eaque nostrum consequuntur recusandae molestiae est numquam, corrupti excepturi repudiandae similique amet temporibus architecto doloremque sunt ipsa.
-          <br /><br />
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis iste quaerat porro labore eaque nostrum consequuntur recusandae molestiae est numquam, corrupti excepturi repudiandae similique amet temporibus architecto doloremque sunt ipsa.
-          <br /><br />
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis iste quaerat porro labore eaque nostrum consequuntur recusandae molestiae est numquam, corrupti excepturi repudiandae similique amet temporibus architecto doloremque sunt ipsa. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis iste quaerat porro labore eaque nostrum consequuntur recusandae molestiae est numquam, corrupti excepturi repudiandae similique amet temporibus architecto doloremque sunt ipsa.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis iste quaerat porro labore eaque nostrum consequuntur recusandae molestiae est numquam, corrupti excepturi repudiandae similique amet temporibus architecto doloremque sunt ipsa.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis iste quaerat porro labore eaque nostrum consequuntur recusandae molestiae est numquam, corrupti excepturi repudiandae similique amet temporibus architecto doloremque sunt ipsa.
+          {data.content}
         </p>
       </div>
     </div>
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,28 +2,51 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
+const getData = async () => {
+  const res = await fetch('http://localhost:3000/api/posts', {
+    cache: "no-store"
+  })
 
-const Blog = () => {
+  if (!res.ok) {
+    throw new Error('Failed to Fetch Data')
+  }
+  return res.json()
+}
+
+export const metadata = {
+  title: 'Our Blog',
+  description: 'Say What you want freely',
+}
+
+
+const Blog = async () => {
+
+  const data = await getData()
+  // const data = []
+
+
   return (
     <div className="">
 
-      <Link className="flex  items-center gap-12 mb-12" href="blog/testID" >
-        <div className="">
-          <Image
-            src="https://images.pexels.com/photos/768474/pexels-photo-768474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt=""
-            width={400}
-            height={250}
-            className="object-cover flex-1"
-          />
-        </div>
-        <div className="flex-1">
-          <h1 className="text-3xl font-bold mb-5">Test</h1>
-          <p className="text-xl text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis cupiditate voluptatibus quibusdam consectetur officiis reiciendis praesentium atque, aliquid voluptatum temporibus, porro libero beatae explicabo, sunt magni sapiente totam perferendis magnam. </p>
-        </div>
-      </Link>
-
-
+      {
+        data.map(post => (
+          <Link key={post._id} className="flex  items-center gap-12 mb-12" href={"blog/"+ post._id } >
+            <div className="">
+              <Image
+                src={post.img}
+                alt=""
+                width={400}
+                height={250}
+                className="object-cover flex-1"
+              />
+            </div>
+            <div className="flex-1">
+              <h1 className="text-3xl font-bold mb-5">{post.title}</h1>
+              <p className="text-xl text-justify">{post.desc}</p>
+            </div>
+          </Link>
+        ))
+      }
     </div>
   )
 }
