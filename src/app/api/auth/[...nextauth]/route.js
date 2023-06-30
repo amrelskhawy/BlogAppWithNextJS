41572a2cdc4458084d1afcac220c96162d8aa03e
Amr Elskhawy <amrelskhawy@gmail.com>
2023-07-01 00:13:48 +0300

    fix some bugs in next auth

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,50 +6,64 @@ import bcrypt from "bcrypt";
 import connect from "@/utils/db";
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET
-        }),
-        CredentialsProvider({
-            id: "credentials",
-            name: "Credentials",
-            async authorize(credentials) {
-                //Check if the user exists.
-                await connect();
+providers: [
+    GoogleProvider({
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET
+    }),
+    CredentialsProvider({
+        id: "credentials",
+        name: "Credentials",
+        async authorize(credentials) {
+            //Check if the user exists.
+        await connect();
 
-                try {
-                    const user = await User.find({
+            console.log('Connected')
+            console.log(credentials.email)
+            console.log(credentials.password)
+
+            try {
+                console.log('Start Try')
+
+                const user = await User.findOne({
                         email: credentials.email,
-                    });
+                });
 
-                    if (user) {
-                        const isPasswordCorrect = await bcrypt.compare(
-                            credentials.password,
-                            user.password
-                        );
+                console.log('Start User Try')
+                console.log(user)
 
-                        if (isPasswordCorrect) {
-                            return user;
-                        } else {
-                            throw new Error("Wrong Credentials!");
-                        }
+                if (user) {
+                    console.log('There is User')
+
+                    const isPasswordCorrect = await bcrypt.compare(
+                        credentials.password,
+                        user.password
+                    );
+
+                    console.log(`pass is : ${isPasswordCorrect}`)
+
+                    if (isPasswordCorrect) {
+                        return user;
+                    } else {
+                        throw new Error("Wrong Credentials!");
                     }
-                    else {
-                        throw new Error("User not found!");
-                    }
-                } catch (err) {
-                    throw new Error(err);
                 }
-            },
-        }),
-    ],
-    pages: {
-        error: "/dashboard"
-    }
+                else {
+                    console.log('Not User')
+                    throw new Error("User not found!");
+                }
+            } catch (err) {
+                throw new Error(err);
+            }
+        },
+    }),
+],
+pages: {
+    error: "/dashboard"
+}
 })
 
 export {
-    handler as GET,
-    handler as POST
+handler as GET,
+handler as POST
 }
