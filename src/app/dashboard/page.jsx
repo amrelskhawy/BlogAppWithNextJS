d3edb49087e42da7caf66924862997eb81d854f6
Amr Elskhawy <amrelskhawy@gmail.com>
2023-06-30 18:40:57 +0300

    updates dashboard page

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,33 +1,30 @@
 'use client'
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
+import {useSession} from "next-auth/react";
+import {useRouter} from "next/navigation";
 
 const Dashboard = () => {
 
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     setIsLoading(true)
 
-  //     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`, { cache: "no-store" })
 
-  //     if (!res.ok) {
-  //       setErr(true)
-  //     }
-  //     setIsLoading(false)
-  //     const data = res.json()
-  //     setData(data)
-  //   }
-  //   getData()
-  // }, [])
   const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-  const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
+  // const { data, error, isLoading } = useSWR('https://jsonplaceholder.typicode.com/posts', fetcher)
 
+  const {data, status} = useSession()
+  const router = useRouter()
 
-  console.log(data);
-  return (
-    <div>Dashboard Page</div>
-  )
+    // if (status === "loading") {
+    //     return <p>Loading...</p>;
+    // }
+    // if (status === "unauthenticated") {
+    //     router?.push("/dashboard/login");
+    // }
+
+    return (
+      <div>Dashboard Page</div>
+    )
 }
 
 export default Dashboard
