66b09060e6eb98591fccb62ea63af453e939b067
Amr Elskhawy <amrelskhawy@gmail.com>
2023-06-30 18:37:15 +0300

    Creates Auth Provider and Adds it to layout root

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,18 +4,16 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import Footer from '@/components/Footer'
 import { ThemeProvider } from './context/ThemeContext'
+import {AuthProvider} from "@/components/AuthProvider/AuthProvider";
 
-
-export const metadata = {
-  title: 'Blog App',
-  description: 'Say What you want freely',
-}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body >
       <ThemeProvider>
+          <AuthProvider>
+
         <div className='container'>
           <Navbar />
             <section>
@@ -23,6 +21,7 @@ export default function RootLayout({ children }) {
             </section>
           <Footer />
         </div>
+          </AuthProvider>
       </ThemeProvider>
       </body>
     </html>
