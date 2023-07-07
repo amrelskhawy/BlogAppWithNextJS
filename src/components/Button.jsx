90e6eea995c52e76c169d4168cb94068941761ed
Amr Elskhawy <amrelskhawy@gmail.com>
2023-07-07 16:05:49 +0300

    edit button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 import React from 'react'
 
-const Button = ({url, text }) => {
+const Button = ({url, text, className, ...other }) => {
   return (
           
-    <Link  className='bg-[#53c284] p-5 cursor-pointer text-white max-w-max font-bold rounded ' href={url}>
+    <Link className={`bg-[#53c284] p-5 cursor-pointer text-white max-w-max font-bold rounded ${className}`} {...other} href={url}>
         <button>
             {text}
         </button>
