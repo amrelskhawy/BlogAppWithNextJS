ed2087c5aab2ca422d083aa574e2a27eea4e96b6
Amr Elskhawy <amrelskhawy@gmail.com>
2023-07-08 00:02:05 +0300

    adds delete request to posts/id route

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -3,13 +3,13 @@ import connect from "@/utils/db"
 import { NextResponse } from "next/server"
 
 
-export const GET = async (request, {params}) => {
-    const {id } = params
+export const GET = async (request, { params }) => {
+    const { id } = params
     // fetch
     try {
 
         await connect()
-        const post = await Post.findById(id )
+        const post = await Post.findById(id)
 
         return new NextResponse(JSON.stringify(post), {
             status: 200
@@ -23,4 +23,26 @@ export const GET = async (request, {params}) => {
             status: 500
         })
     }
+}
+
+
+export const DELETE = async (request, { params }) => {
+    const { id } = params
+
+    try {
+
+        await connect()
+
+        await Post.findByIdAndDelete(id)
+
+        return new NextResponse("Post has been Deleted", {
+            status: 200
+        })
+
+    } catch (error) {
+        return new NextResponse("Database Error", {
+            status: 500
+        })
+    }
+
 }
